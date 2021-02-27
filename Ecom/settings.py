4680ea783ceeaa868ff0587f467750644c073ae9
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-02-27 22:48:37 +0200

    ✨ Add static files and media settings

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -124,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'resources'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
