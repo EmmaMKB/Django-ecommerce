ec4093b4a44eaa1e206a5ab6c3485105ab854728
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-02-27 17:44:08 +0200

    ✨ Add Product model

--- Ecommerce/models.py
+++ Ecommerce/models.py
@@ -15,6 +15,33 @@ class Category(models.Model):
         db_table = 'category'
 
 
+class Product(models.Model):
+    uid = models.CharField(unique=True, max_length=255, blank=True, null=True, default=uuid.uuid4)
+    name = models.CharField(max_length=256, blank=True, null=True)
+    description = models.CharField(max_length=2048, blank=True, null=True)
+    tags = models.CharField(max_length=512, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    in_stock = models.BooleanField(default=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    image = models.FileField(upload_to='products/%Y/%m/%d/', default=None)
+    subcategory = models.ForeignKey('Subcategory', models.DO_NOTHING, db_column='subcategory', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'product'
+
+class Subcategory(models.Model):
+    uid = models.CharField(unique=True, max_length=255, blank=True, null=True, default=uuid.uuid4)
+    name = models.CharField(max_length=128, blank=True, null=True)
+    description = models.CharField(max_length=512, blank=True, null=True)
+    image = models.FileField(upload_to='sub_categories/%Y/%m/%d/', default=None)
+    category = models.ForeignKey(Category, models.CASCADE, db_column='category', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'subcategory'
+
+
 class User(models.Model):
 
     uid = models.CharField(unique=True, max_length=255, blank=True, null=True, default=uuid.uuid4)
