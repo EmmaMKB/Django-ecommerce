9af1e00b512d6c97e5d7aaaf63a4b1ed20f1d55f
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-03-01 13:32:39 +0200

    ✨ Load subcategories from db

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from Ecommerce.dao import subcategory
+
+SubcategoryDao = subcategory.SubCategoryDao()
 
 
 def index(request):
-
-    return render(request, 'index.html')
+    
+    subcategories = SubcategoryDao.get_all()
+    return render(
+        request, 
+        'index.html', {
+            'subcategories': subcategories
+        })
