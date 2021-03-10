abbaa438842fe11a312a966181b27f3ed18dceb4
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-03-10 11:44:07 +0200

    ✨ Create custom user model

--- Ecommerce/models.py
+++ Ecommerce/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 import uuid
+from .managers import CustomUserManager
 
 
 class Basket(models.Model):
     uid = models.CharField(unique=True, max_length=255, blank=True, null=True, default=uuid.uuid4)
-    user = models.ForeignKey('User', models.CASCADE, db_column='user', blank=True, null=True)
+    user = models.ForeignKey('Person', models.CASCADE, db_column='user', blank=False, null=False)
     created_at = models.DateTimeField(blank=True, null=True, default=timezone.now)
     is_ordered = models.BooleanField(blank=True, null=True, default=False)
 
@@ -40,7 +42,7 @@ class Newsletter(models.Model):
 class Order(models.Model):
     uid = models.CharField(max_length=255, blank=True, null=True, default=uuid.uuid4)
     reference_code = models.CharField(max_length=256, blank=True, null=True)
-    user = models.CharField(max_length=256, blank=True, null=True)
+    user = models.ForeignKey('Person', models.CASCADE, db_column='user', blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True, default=timezone.now)
     state = models.CharField(max_length=128, blank=True, null=True)
     payment_method = models.CharField(max_length=128, blank=False, null=False, default="")
@@ -80,7 +82,7 @@ class ProductToBasket(models.Model):
 
     uid = models.CharField(max_length=255, blank=True, null=True, default=uuid.uuid4)
     product = models.ForeignKey(Product, models.DO_NOTHING, db_column='product', blank=True, null=True)
-    user = models.ForeignKey('User', models.CASCADE, db_column='user', blank=True, null=True)
+    user = models.ForeignKey('Person', models.CASCADE, db_column='user', blank=True, null=True)
     basket = models.ForeignKey(Basket, models.DO_NOTHING, db_column='basket', blank=True, null=True)
     qty = models.IntegerField(blank=True, null=True)
     is_valid = models.BooleanField(blank=True, null=True, default=True)
@@ -105,25 +107,23 @@ class Subcategory(models.Model):
         db_table = 'subcategory'
 
 
-class User(models.Model):
+class Person(AbstractUser):
 
+    username = None
     uid = models.CharField(unique=True, max_length=255, blank=True, null=True, default=uuid.uuid4)
-    firstname = models.CharField(max_length=256, blank=False, null=False, default="")
-    name = models.CharField(max_length=256, blank=False, null=False, default="")
-    email = models.CharField(max_length=256, blank=False, null=False, default="")
-    password = models.CharField(max_length=512, blank=False, null=False, default="")
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=32, blank=True, null=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    is_active = models.BooleanField(default=False)
 
-    class Meta:
-        db_table = 'user'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
 
 class Wishlist(models.Model):
     uid = models.CharField(max_length=255, blank=True, null=True, default=uuid.uuid4)
     product = models.ForeignKey(Product, models.DO_NOTHING, db_column='product', blank=True, null=True)
-    user = models.ForeignKey('User', models.CASCADE, db_column='user', blank=True, null=True)
+    user = models.ForeignKey('Person', models.CASCADE, db_column='user', blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True, default=timezone.now)
     is_active = models.IntegerField(blank=True, null=True, default=True)
 
