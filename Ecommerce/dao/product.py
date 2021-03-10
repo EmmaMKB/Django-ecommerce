aba7d243f66c66531cca47c6ca9487c2ee3d3b00
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-03-10 16:55:47 +0200

    ✨ Add get all products method

--- Ecommerce/dao/product.py
+++ Ecommerce/dao/product.py
@@ -4,6 +4,10 @@ from ..models import Product
 class ProductDao:
 
     @staticmethod
+    def get_all():
+        return Product.objects.all()
+
+    @staticmethod
     def get_lasts(number=8):
 
         return Product.objects.all().filter(in_stock=1).order_by('-created_at')[:number]
